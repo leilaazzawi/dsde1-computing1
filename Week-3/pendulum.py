5f63b0252ff8c926a582bfb405b8c1e6a2acfefd
leilaazzawi <leila.al-azzawi19@ic.ac.uk>
2019-11-11 19:35:16 +0000

    completed fuzzy.py

--- Week-3/pendulum.py
+++ Week-3/pendulum.py
@@ -5,12 +5,10 @@ g = input()
 
 import math
 def time_period(l, g):
-        if type(l) != float or type(g) != float:
-                raise TypeError
-                print('please enter two numbers')
+        if isinstance(l,str) or isinstance(g, str):
+                raise TypeError ('please enter two numbers')
         if l == 0 or g == 0:
-                raise ValueError
-                print('please enter non-zero numbers')
+                raise ValueError ('please enter non-zero numbers')
         T = math.pi*math.pow(l/g, 1/2)
         print ('Time Period = ' + str(T))
         return T
--- Week-4/fuzzy.py
+++ Week-4/fuzzy.py
@@ -6,27 +6,27 @@ Lint this file using PyLint.
 
 # This function does some maths on three numbers.
 def maths(input_a, input_b, input_c):
+    '''does maths on three numbers'''
     result = input_a * 3 - input_b + input_c
     return result
 
 # This function returns True or False.
 def choices(question):
-    if(question == True):
+    '''returns true or false'''
+    if question:
         return True
-    else:
-        return False
+    return False
 
 
 def main():
-    this_variable = 'useless'
-    
+    '''takes in the numbers'''
     # first function takes three numbers
-    answer = maths(3, 9 , 2.3)
+    answer = maths(3, 9, 2.3)
     print(answer)
 
     # second function takes a True or False
-    newAnswer = choices(True)
-    print(newAnswer)
+    new_answer = choices(True)
+    print(new_answer)
 
 if __name__ == '__main__':
     main()
